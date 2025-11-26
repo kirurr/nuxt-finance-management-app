9599a264068ca19cf1923cc5db46957398714556
kirurr <timon09df@gmail.com>
2025-11-26 14:17:46 +0300

    signout alert dialog

--- app/components/auth/SignOut.vue
+++ app/components/auth/SignOut.vue
@@ -2,5 +2,23 @@
 import { handleSignOut } from "~/lib/auth";
 </script>
 <template>
-	<Button variant="outline" @click="handleSignOut">Sign out</Button>
+  <AlertDialog>
+    <AlertDialogTrigger as-child>
+      <Button variant="outline">Sign out</Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Sign out</AlertDialogTitle>
+        <AlertDialogDescription>
+          Are you sure you want to sign out?
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction as-child>
+          <Button variant="destructive" @click="handleSignOut">Sign out</Button>
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
 </template>
